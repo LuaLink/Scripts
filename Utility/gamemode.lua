5b3c8861c450d97831de8c87c03816ffdc83c342
Saturn <github.vcnmd@passinbox.com>
2025-04-13 04:45:15 +0000

    Update gamemode.lua

--- Utility/gamemode.lua
+++ Utility/gamemode.lua
@@ -1,17 +1,14 @@
--- Helpful command aliases for gamemodes
-local GameMode = import "org.bukkit.GameMode"
-local MiniMessage = import "net.kyori.adventure.text.minimessage.MiniMessage"
+local GameMode = java.import "org.bukkit.GameMode"
 
--- Helper function to set the game mode for a player or all players
 local function setGameMode(player, mode)
     
     if player == "@a" then
-        for _, onlinePlayer in ipairs(totable(script.getServer():getOnlinePlayers())) do
+        for _, onlinePlayer in ipairs(totable(server:getOnlinePlayers())) do
             onlinePlayer:setGameMode(mode)
         end
     else
         if type(player) == "string" then
-            player = script.getServer():getPlayer(player)
+            player = server:getPlayer(player)
         end
         if player == nil then
             return "<red>Player not found"
@@ -35,9 +32,13 @@ local function sendMessage(sender, target, mode)
     end
 end
 
--- Register the /gmc command
-script.registerSimpleCommand(function(sender, args)
-    local playerName = args[1]
+
+-- GMC
+script:registerCommand(function(sender, args)
+    local playerName;
+    if #args > 0 then
+        playerName = args[1]
+    end
     if playerName ~= nil and playerName ~= sender:getName() and not sender:hasPermission("minecraft.command.gamemode.creative.other") then
         sender:sendRichMessage("<red>You do not have permission to change the gamemode of other players")
         return
@@ -55,34 +56,42 @@ end, {
     permission = "minecraft.command.gamemode.creative"
 })
 
--- Register the /gms command
-script.registerSimpleCommand(function(sender, args)
-    local playerName = args[1]
+-- GMS
+script:registerCommand(function(sender, args)
+    local playerName;
+    if #args > 0 then
+        playerName = args[1]
+    end
     if playerName ~= nil and playerName ~= sender:getName() and not sender:hasPermission("minecraft.command.gamemode.survival.other") then
         sender:sendRichMessage("<red>You do not have permission to change the gamemode of other players")
         return
     end
-    local result = setGameMode(playerName or sender,GameMode.SURVIVAL)
+    local result = setGameMode(playerName or sender, GameMode.SURVIVAL)
     if result then
         sender:sendRichMessage(result)
     else
         sendMessage(sender, playerName or sender:getName(), "Survival")
     end
-end, { 
+end, {
     name = "gms",
     description = "Set game mode to Survival",
     usage = "/gms [player]",
     permission = "minecraft.command.gamemode.survival"
 })
 
--- Register the /gmsp command
-script.registerSimpleCommand(function(sender, args)
-    local playerName = args[1]
+-- GMSP
+
+script:registerCommand(function(sender, args)
+    local playerName;
+    if #args > 0 then
+        playerName = args[1]
+    end
+
     if playerName ~= nil and playerName ~= sender:getName() and not sender:hasPermission("minecraft.command.gamemode.spectator.other") then
         sender:sendRichMessage("<red>You do not have permission to change the gamemode of other players")
         return
     end
-    local result = setGameMode(playerName or sender,GameMode.SPECTATOR)
+    local result = setGameMode(playerName or sender, GameMode.SPECTATOR)
     if result then
         sender:sendRichMessage(result)
     else
@@ -95,14 +104,19 @@ end, {
     permission = "minecraft.command.gamemode.spectator"
 })
 
--- Register the /gma command
-script.registerSimpleCommand(function(sender, args)
-    local playerName = args[1]
+-- GMA
+
+script:registerCommand(function(sender, args)
+    local playerName;
+    if #args > 0 then
+        playerName = args[1]
+    end
+
     if playerName ~= nil and playerName ~= sender:getName() and not sender:hasPermission("minecraft.command.gamemode.adventure.other") then
         sender:sendRichMessage("<red>You do not have permission to change the gamemode of other players")
         return
     end
-    local result = setGameMode(playerName or sender,GameMode.ADVENTURE)
+    local result = setGameMode(playerName or sender, GameMode.ADVENTURE)
     if result then
         sender:sendRichMessage(result)
     else
